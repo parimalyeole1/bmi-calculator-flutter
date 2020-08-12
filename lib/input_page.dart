a393b4cff403e46350447510cef11ee80f7ccb39
Parimal Y <parimal.yeole@dkatalis.com>
2020-08-12 21:58:24 +0530

    refactored reusable to include gesture

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -33,38 +33,34 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
+                    child: ReusableCard(
+                      onPress: () {
                         setState(() {
                           selectedGender = Gender.male;
                         });
                       },
-                      child: ReusableCard(
-                        colour: selectedGender == Gender.male
-                            ? activeCardColour
-                            : inActiveCardColour,
-                        cardChild: IconContent(
-                          iconText: 'MALE',
-                          cardIcon: FontAwesomeIcons.mars,
-                        ),
+                      colour: selectedGender == Gender.male
+                          ? activeCardColour
+                          : inActiveCardColour,
+                      cardChild: IconContent(
+                        iconText: 'MALE',
+                        cardIcon: FontAwesomeIcons.mars,
                       ),
                     ),
                   ),
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
+                    child: ReusableCard(
+                      onPress: () {
                         setState(() {
                           selectedGender = Gender.female;
                         });
                       },
-                      child: ReusableCard(
-                        colour: selectedGender == Gender.female
-                            ? activeCardColour
-                            : inActiveCardColour,
-                        cardChild: IconContent(
-                          iconText: 'FEMALE',
-                          cardIcon: FontAwesomeIcons.venus,
-                        ),
+                      colour: selectedGender == Gender.female
+                          ? activeCardColour
+                          : inActiveCardColour,
+                      cardChild: IconContent(
+                        iconText: 'FEMALE',
+                        cardIcon: FontAwesomeIcons.venus,
                       ),
                     ),
                   )
